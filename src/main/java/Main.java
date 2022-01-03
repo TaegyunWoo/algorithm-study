7ba01833b6e81ccef0bb7710dd3106052656794e
TaegyunWoo <76861101+TaegyunWoo@users.noreply.github.com>
2022-01-03 17:29:08 +0900

    Add Problem 큰 수의 법칙

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import greedy.거스름돈;
+import greedy.큰_수의_법칙;
 
 import java.util.Scanner;
 
@@ -6,12 +7,24 @@ public class Main {
     public static void main(String[] args) {
         //입력
         Scanner scn = new Scanner(System.in);
-        System.out.print("input: ");
-        int input = scn.nextInt();
+
+        System.out.println("Input");
+
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int k = scn.nextInt();
+
+        int[] arr = new int[n];
+
+        scn.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
 
         TimeCheck.start();
 
-        int answer = 거스름돈.solution2(input);
+        int answer = 큰_수의_법칙.solution3(n, m, k, arr);
         System.out.println("answer = " + answer);
 
         TimeCheck.end();
